3154ca00aae1b770b390cb96f4de5f779cb47aa1
nubelsondev <nubelsondev@hotmail.com>
2019-12-19 06:23:36 +0000

    Creating Shared Page Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+
 const App = () => {
     return (
         <div>
+            <Header />
             <h1>Hello!</h1>
             <h2>I', Nubelson, a Full-Stack developer living in Portugal.</h2>
             <p>
                 Need a developer? <Link to="/contact">Contact me</Link>
             </p>
+
+            <Footer />
         </div>
     )
 }

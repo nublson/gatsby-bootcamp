c43a90c87aa535c2c8987f28cbb7c0bf6c7adb5a
nubelsondev <nubelsondev@hotmail.com>
2020-01-02 05:32:08 +0000

    Getting Started With Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,12 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
     ],
 }
